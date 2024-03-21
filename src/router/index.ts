0ec1418a3ec7ead8ea457ba622b077fc95aae5ea
L0st1 <hunjin360@gmail.com>
2024-03-21 09:49:13 +0800

    fix：主应用路由未匹配微应用路由引发的Vue Router warn
    动态路由匹配

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import EmptyView from "../views/HomeView.vue";
+import MicroAppRoute from "@/components/MicroAppRouteComponent.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +12,13 @@ const routes: Array<RouteRecordRaw> = [
     path: "/about",
     name: "base-about",
     component: () => import("../views/AboutView.vue"),
+  },
+  // 处理控制台警告 [Vue Router warn]: No match found for location with path
+  // 要求当前接入的所有微应用都是 "app-" 开头
+  {
+    path: "/app-:appName(.*)",
+    name: 'MicroApp',
+    component: MicroAppRoute, // 一个空内容的组件
   },
 ];
 

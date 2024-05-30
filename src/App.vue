4c3f0e70336dab2f13dccfac56c52579104d0747
L0st1 <hunjin360@gmail.com>
2024-05-30 17:55:52 +0800

    chore：路由跳转处理；调整

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <!-- <nav class="base-nav">
-    <router-link to="/about">About</router-link> |
-    <router-link to="/app-vue-hash/">app1</router-link> |
-    <router-link to="/app-vue-history/">app2</router-link> |
-    <router-link to="/app-vue3">app-vue3</router-link> |
-    <a href="/about">a标签跳转</a>
-    <el-button @click="routerReplace">点击跳转</el-button>
-  </nav> -->
   <NavView></NavView>
   <div id="appContainer"></div>
+  <aside class="base-aside"></aside>
   <router-view />
 </template>
 <script setup lang="ts">
-// import { watch } from "vue";
-// import { useRouter } from "vue-router";
 import NavView from "@/views/NavView.vue";
-// const router = useRouter();
 // 主应用点击 router-link 进行跳转时，无异常
-// 进入子应用，点击子应用的 router-link 后，再次点击主应用的 router-link 不会发生跳转——
-// 这是因为点击子应用路由后，浏览器的history发生了变更，但主应用路由实例并未得到通知，所以再使用router-link发动变更时会出错
-// 使用a标签不会，但a标签会触发重新导航（影响用户体验，与现在前端路由理念相悖）
+// 进入子应用，点击子应用的 router-link 后，再次点击主应用的 router-link 不会发生跳转—— 因为在子应用发生路由跳转时，是基于子应用的router-base跳转
+// 使用a标签不会，但a标签会触发重新导航（影响用户体验，与现在前端路由理念相悖），但可以为其绑定click事件并使用History API
 
-// 解决如下：
-// const routerReplace = () => {
-//   router.replace("/app-vue3");
-// };
 </script>
 <style lang="scss">
 #base-app {
@@ -35,18 +20,24 @@ import NavView from "@/views/NavView.vue";
   color: #2c3e50;
   height: 100vh;
   width: 100vw;
+  display: grid;
+	grid-template-rows: repeat(6, 1fr);
+	grid-template-columns: repeat(6, 1fr);
+	gap: 0;
 }
-
 .base-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  grid-area: 1 / 1 / 2 / 4;
+  border-bottom: 1px solid violet;
+}
+#appContainer {
+  width: calc(100vw - 500px);
+  height: 100%;
+  grid-area: 2 / 1 / 7 / 3;
+}
+.base-aside {
+  min-width: 499px;
+  height: 100%;
+  grid-area: 2 / 3 / 7 / 4;
+  border-left: 1px solid violet;
 }
 </style>
--- src/views/NavView.vue
+++ src/views/NavView.vue
@@ -1,32 +1,47 @@
 <template>
   <nav class="base-nav">
-    <RouterLink v-for="{ code, path, id } in routerTree" :key="id" :to="path" replace>{{ code }} | </RouterLink>
+    <!-- <RouterLink v-for="{ code, path, id } in routerTree" :key="id" :to="path" replace class="router-link">{{ code }} | </RouterLink> -->
+    <p v-for="{ code, path, id, appName } in routerTree" :key="id" class="base-nav__item">
+      <a @click="handleRouter(path)" class="base-nav__item--link">{{ code }}</a><br />
+      <span class="base-nav__item--app">{{ appName }}</span>
+    </p>
   </nav>
 </template>
 <script lang="ts" setup>
 const routerTree = [
-  { code: "about", id: 1, path: "/app-vue3/about"},
-  { code: "interserctionOb", id: 2, path: "/app-vue3/useInterserctionOb"},
-  { code: "AnchorByElement", id: 3, path: "/app-vue3/AnchorByElement"},
-  { code: "ElementTest", id: 4, path: "/app-vue3/test"},
-  { code: "", id: 5, path: "/app-vue3"},
-  { code: "", id: 6, path: "/app-vue3"},
-  { code: "", id: 7, path: "/app-vue3"},
-  { code: "", id: 8, path: "/app-vue3"},
-]
+  { code: "about", id: 1, path: "/app-vue3/about", appName: "app-vue3" },
+  { code: "interserctionOb", id: 2, path: "/app-vue3/useInterserctionOb", appName: "app-vue3" },
+  { code: "AnchorByElement", id: 3, path: "/app-vue3/AnchorByElement", appName: "app-vue3" },
+  { code: "ElementTest", id: 4, path: "/app-vue3/test", appName: "app-vue3" },
+  // { code: "BaseAbout", id: 5, path: "/about", appName: "base" },
+  { code: "AnotherApp", id: 6, path: "/app-vue-history/about", appName: "app-vue2" },
+  // { code: "", id: 7, path: "/app-vue3", appName: "app-vue3" },
+  // { code: "", id: 8, path: "/app-vue3", appName: "app-vue3" },
+];
+const handleRouter = (path: string) => {
+  window.history.pushState({}, '', path);
+}
 </script>
 <style scoped lang="scss">
 .base-nav {
   font-size: 20px;
   padding: 34px;
+  text-align: start;
+  display: flex;
+  gap: 16px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  &__item {
+    &--link {
+      font-weight: bold;
+      color: #2c3e50;
+    }
 
-    &.router-link-exact-active {
-      color: #42b983;
+    &--app {
+      color: color-mix(in srgb, blue 30%, red 50%);
+      font-size: 14px;
     }
   }
+
+
 }
 </style>
